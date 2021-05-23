f37695f7ba01e8778a2c4a81a85a194d7425ca90
nbvinh <vinh230620@gmail.com>
2021-05-23 17:50:57 +0700

    update trai nghiem

--- src/components/Details/ExpericenceDetails/index.js
+++ src/components/Details/ExpericenceDetails/index.js
@@ -1,37 +1,41 @@
 import React from 'react';
-import { View, Text, SafeAreaView, StatusBar, ScrollView, Image, TouchableOpacity } from 'react-native';
+import { View, Text, SafeAreaView, StatusBar, ScrollView, Image, TouchableOpacity, ImageBackground, FlatList } from 'react-native';
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { scale } from 'react-native-size-matters';
+import { scaleW } from '../../../config/Ultils';
+import TextHome from '../../TextHome';
 import styles from './styles';
 
 export default function ExperienceDetails({ navigation, route }) {
     const item = route.params.item
-    console.log('item', item)
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: '#E5E5E5' }}>
             <StatusBar backgroundColor='transparent' translucent animated barStyle={"light-content"} />
             <ScrollView style={{ flex: 1 }}>
-                <Image
+                <ImageBackground
                     source={{ uri: item.img }}
                     style={styles.imgheader}
-                />
-                <Image
-                    source={require('./../../../img/ChevronRight.png')}
-                    style={styles.iconRight}
-                />
-                <Text style={styles.label}>{item.text}</Text>
-                <View style={{
-                    flexDirection: 'row', position: 'absolute',
-                    left: scale(16),
-                    bottom: scale(16),
-                }}>
-                    <Image
-                        source={require('./../../../img/location1.png')}
-                        style={styles.location}
-                    />
-                    <Text style={styles.address}>{item.text1} </Text>
-                </View>
-                <TouchableOpacity>
+                >
+                    <TouchableOpacity style={styles.Back} onPress={() => navigation.goBack()}>
+                        <Image
+                            source={require('./../../../img/ChevronRight.png')}
+                            style={styles.iconRight}
+                        />
+                    </TouchableOpacity>
+                    <Text style={styles.label1}>{item.text}</Text>
+                    <View style={{
+                        flexDirection: 'row', position: 'absolute',
+                        left: scale(16),
+                        bottom: scale(16),
+                    }}>
+                        <Image
+                            source={require('./../../../img/location1.png')}
+                            style={styles.location}
+                        />
+                        <Text style={styles.address}>{item.text1} </Text>
+                    </View>
+                </ImageBackground>
+                <TouchableOpacity style={styles.map}>
                     <MapView
                         provider={PROVIDER_GOOGLE} // remove if not using Google Maps
                         style={styles.mapaddress}
@@ -55,6 +59,61 @@ export default function ExperienceDetails({ navigation, route }) {
                         </Marker>
                     </MapView>
                 </TouchableOpacity>
+                <View style={styles.Introduce}>
+                    <Image
+                        source={item.introduce1}
+                        style={styles.introduceimg}
+                    />
+                    <Image
+                        source={item.introduce2}
+                        style={styles.introduceimg}
+                    />
+                    <Image
+                        source={item.introduce3}
+                        style={styles.introduceimg}
+                    />
+                    <TouchableOpacity style={styles.plus}>
+                        <Image
+                            source={require('../../../img/plus.png')}
+                            style={styles.imgHeader}
+                        />
+                    </TouchableOpacity>
+                </View>
+                <View style={{ marginHorizontal: scaleW(16) }}>
+                    <Text style={styles.content1}>{item.content1}</Text>
+                    <Text style={styles.label}>{item.label1}</Text>
+                    <Text style={[styles.label, { marginVertical: scaleW(10) }]}>{item.label2}</Text>
+                    <Text style={[styles.label, { marginLeft: scaleW(15) }]}>{item.label3}</Text>
+                    <Text style={[styles.label, { marginLeft: scaleW(15) }]}>{item.label4}</Text>
+                    <Text style={[styles.label, { marginLeft: scaleW(15), marginBottom: scaleW(20) }]}>{item.label5}</Text>
+                    <Text style={styles.content1}>{item.content2}</Text>
+                    <Text style={styles.label}>{item.label6}</Text>
+                    <Text style={styles.label}>{item.label7}</Text>
+                    <Text style={[styles.content1, { marginTop: scaleW(20) }]}>{item.content3}</Text>
+                    <Text style={styles.text}>{item.text1}</Text>
+                    <Text style={styles.label}>{item.label8}</Text>
+                    <Text style={[styles.text, { marginTop: scaleW(20) }]}>{item.text2}</Text>
+                    <Text style={styles.label}>{item.label9}</Text>
+                    <Text style={[styles.text, { marginTop: scaleW(20) }]}>{item.text3}</Text>
+                    <Text style={styles.label}>{item.label10}</Text>
+                    <Text style={[styles.text, { marginTop: scaleW(20) }]}>{item.text4}</Text>
+                    <Text style={styles.label}>{item.label11}</Text>
+                </View>
+                <TextHome text1={'Địa điểm đề xuất'} text2={'Xem Thêm >'}
+                    onSeeMore={() => {
+                        dispatch({ type: 'TYPE', typeHotel: "Hotel" })
+                        navigation.navigate("HomeStack", { screen: 'SeeMoreHotel' })
+                    }}
+                />
+                <ScrollView horizontal={true} style={{ marginHorizontal: scaleW(16) }}>
+                    {item.offer && item.offer.map((e, index) => {
+                        return (
+                            <ImageBackground key={index} source={e.img1} style={styles.image}>
+                                <Text style={styles.textimg1}>{e.textimg1}</Text>
+                            </ImageBackground>
+                        )
+                    })}
+                </ScrollView>
             </ScrollView>
         </SafeAreaView>
     );
--- src/components/Details/ExpericenceDetails/styles.js
+++ src/components/Details/ExpericenceDetails/styles.js
@@ -1,41 +1,100 @@
-import { StyleSheet } from "react-native"
-import { scale } from "react-native-size-matters"
-
+import { Dimensions, StyleSheet } from "react-native"
+import { scaleW } from "../../../config/Ultils"
+const { width, height } = Dimensions.get('screen')
 const styles = StyleSheet.create({
+    map: {
+        marginTop: scaleW(20),
+        marginBottom: scaleW(16)
+    },
     imgheader: {
         width: '100%',
-        height: scale(240)
+        height: scaleW(240)
     },
     iconRight: {
-        width: scale(6.61),
-        height: scale(12),
+        width: "100%",
+        height: "100%"
+    },
+    Back: {
+        width: scaleW(6.61),
+        height: scaleW(12),
         position: 'absolute',
-        top: scale(48),
-        left: scale(16),
+        top: scaleW(48),
+        left: scaleW(16),
         zIndex: 1
     },
-    label: {
+    label1: {
         position: 'absolute',
-        bottom: scale(36),
-        left: scale(16),
-        fontSize: scale(18),
+        bottom: scaleW(36),
+        left: scaleW(16),
+        fontSize: scaleW(18),
         fontWeight: '700',
         color: 'white'
     },
     address: {
         color: 'white',
-        fontSize: scale(10),
+        fontSize: scaleW(10),
         fontWeight: '400'
     },
     location: {
-        width: scale(7.24),
-        height: scale(10),
-        marginRight: scale(7.38)
+        width: scaleW(7.24),
+        height: scaleW(10),
+        marginRight: scaleW(7.38)
     },
     mapaddress: {
         width: '100%',
-        height: scale(140)
+        height: scaleW(140)
     },
-    
+    Introduce: {
+        marginBottom: scaleW(16),
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+    introduceimg: {
+        width: (width - scaleW(64)) / 3,
+        height: (width - scaleW(64)) / 3,
+        marginHorizontal: scaleW(8)
+    },
+    plus: {
+        position: 'absolute',
+        bottom: (width - scaleW(92)) / 6,
+        right: (width - scaleW(30)) / 6,
+        height: scaleW(17),
+        width: scaleW(17),
+    },
+    imgHeader: {
+        width: '100%',
+        height: "100%"
+    },
+    content1: {
+        fontSize: scaleW(14),
+        fontWeight: 'bold',
+        color: '#000000',
+        marginBottom: scaleW(8)
+    },
+    label: {
+        fontWeight: '400',
+        fontSize: scaleW(12),
+        lineHeight: scaleW(15.68)
+    },
+    text: {
+        fontWeight: '700',
+        fontSize: scaleW(12),
+        lineHeight: scaleW(15.68),
+        marginBottom: scaleW(15),
+        marginLeft: scaleW(8)
+    },
+    image: {
+        width: scaleW(150),
+        height: scaleW(200),
+        marginRight: scaleW(16),
+        borderRadius: scaleW(5),
+        marginBottom: scaleW(55)
+    },
+    textimg1: {
+        position: 'absolute',
+        bottom: scaleW(9),
+        left: scaleW(10),
+        color: '#FFFFFF'
+    }
 })
 export default styles
